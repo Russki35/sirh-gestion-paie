820d0b8e260eec38d95a9d56be4accc89b606f93
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-30 17:16:47 +0100

    tp1 ok

---
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package dev.paie.service;
+
+import dev.paie.entite.BulletinSalaire;
+import dev.paie.entite.ResultatCalculRemuneration;
+
+
+/**
+ * @author ETY8
+ *
+ */
+public class CalculerRemunerationServiceSimple implements CalculerRemunerationService {
+
+	/* (non-Javadoc)
+	 * @see dev.paie.service.CalculerRemunerationService#calculer(dev.paie.entite.BulletinSalaire)
+	 */
+	@Override
+	public ResultatCalculRemuneration calculer(BulletinSalaire bulletin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
